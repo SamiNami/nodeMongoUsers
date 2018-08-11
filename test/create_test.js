eafff67adfbd09617d33f94ae2d64039f2b3bd31
sami peralahti <sami.peralahti@vainu.io>
2018-08-11 11:53:36 -0700

    very simple test using mocha

---
+++ test/create_test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const User = require('../src/User');
+
+describe('Creating records', () => {
+    it('saves a user', () => {
+        const joe = new User({ name: 'Joe' });
+        joe.save()
+    });
+});
