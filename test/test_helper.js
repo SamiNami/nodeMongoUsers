2474370225ae1c3eff08b76870f07d0919c0e9d1
sami peralahti <sami.peralahti@vainu.io>
2018-08-11 14:08:00 -0700

    made sure that the connection is setup before running tests

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,11 +4,16 @@ mongoose.connect(
     'mongodb://localhost:27017/users_test',
     { useNewUrlParser: true }
 );
-mongoose.connection
-    .once('open', () => console.log('Good to go!'))
-    .on('error', error => {
-        console.warn('Warning', error);
-    });
+
+// make sure that the connection is set up before running tests
+before(done => {
+    mongoose.connection
+        .once('open', () => console.log('Good to go!'))
+        .on('error', error => {
+            console.warn('Warning', error);
+        });
+    done();
+});
 
 beforeEach(done => {
     mongoose.connection.collections.users.drop(() => {
