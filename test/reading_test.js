56c8ec9d4dd12ce9cd3a8b5aececb62175294c3b
sami peralahti <sami.peralahti@vainu.io>
2018-08-11 16:28:12 -0700

    findById

--- test/reading_test.js
+++ test/reading_test.js
@@ -14,7 +14,14 @@ describe('Reading user out of the database', () => {
     it('finds all users with a name of Joe', done => {
         User.find({ name: 'Joe' }).then(users => {
             assert(users[0].id === joe.id);
+            done();
         });
-        done();
+    });
+
+    it('finds a user with a particular id', done => {
+        User.findById(joe.id).then(user => {
+            assert(user.name === 'Joe');
+            done();
+        });
     });
 });
