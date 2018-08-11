61656ac1391514cfb9ee4879311f16b30fe31da3
sami peralahti <sami.peralahti@vainu.io>
2018-08-11 12:18:41 -0700

    db is now emptied before running tests

--- test/create_test.js
+++ test/create_test.js
@@ -4,6 +4,6 @@ const User = require('../src/User');
 describe('Creating records', () => {
     it('saves a user', () => {
         const joe = new User({ name: 'Joe' });
-        joe.save()
+        joe.save();
     });
 });
