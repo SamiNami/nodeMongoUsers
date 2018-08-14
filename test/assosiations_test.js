153e89d990038241ec520b8ebff35e162f2f2aa5
sami peralahti <sami.peralahti@vainu.io>
2018-08-14 12:41:24 -0700

    using populate to load up the assosiated blogpost

--- test/assosiations_test.js
+++ test/assosiations_test.js
@@ -9,24 +9,26 @@ describe('Assosiations', () => {
     beforeEach(done => {
         joe = new User({ name: 'Joe' });
         blogPost = new BlogPost({
-            title: 'Js is Great',
+            title: 'JS is Great',
             content: 'Yep it really is'
         });
         comment = new Comment({ content: 'Congratz on great post' });
-        // references / assosiations 
+        // references / assosiations
         joe.blogPosts.push(blogPost);
         blogPost.comments.push(comment);
         // set a reference in the comment to joe;
         comment.user = joe;
 
-        Promise.all([joe.save(), blogPost.save()], comment.save()).then(() => {
+        Promise.all([joe.save(), blogPost.save(), comment.save()]).then(() => {
             done();
         });
     });
-    it.only('it saves a relation between a user and a blogpost', done => {
-        User.findOne({ name: 'Joe' }).then(user => {
-            console.log(user);
-            done();
-        });
+    it('it saves a relation between a user and a blogpost', done => {
+        User.findOne({ name: 'Joe' })
+            .populate('blogPosts')
+            .then(user => {
+                assert(user.blogPosts[0].title === 'JS is Great');
+                done();
+            });
     });
 });
