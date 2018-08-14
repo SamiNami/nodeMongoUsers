2d04fe691de59672f79e7d41436609f7eb3f8380
sami peralahti <sami.peralahti@vainu.io>
2018-08-13 19:24:25 -0700

    work on first assosiation test

--- test/assosiations_test.js
+++ test/assosiations_test.js
@@ -5,18 +5,28 @@ const BlogPost = require('../src/BlogPost');
 
 describe('Assosiations', () => {
     let joe, blogPost, comment;
+
     beforeEach(done => {
-        joe = new User({ name: 'joe' });
+        joe = new User({ name: 'Joe' });
         blogPost = new BlogPost({
             title: 'Js is Great',
             content: 'Yep it really is'
         });
         comment = new Comment({ content: 'Congratz on great post' });
 
-        joe.blogPosts.push(blogPosts);
+        joe.blogPosts.push(blogPost);
         blogPost.comments.push(comment);
         // set a reference in the comment to joe;
         comment.user = joe;
+
+        Promise.all([joe.save(), blogPost.save()], comment.save()).then(() => {
+            done();
+        });
     });
-    it('can create a subdocument', done => {});
+    it.only('it saves a relation between a user and a blogpost', done => {
+        User.findOne({ name: 'Joe' }).then(user => {
+            console.log(user);
+            done();
+        });
+    });
 });
