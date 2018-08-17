b09c2cef070726d6e068c5d8fc4afdb333cdcaf3
sami peralahti <sami.peralahti@vainu.io>
2018-08-16 21:58:50 -0700

    sort skip and limit

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,13 +2,18 @@ const assert = require('assert');
 const User = require('../src/User');
 
 describe('Reading user out of the database', () => {
-    let joe;
+    let joe, maria, alex, zach;
 
     beforeEach(done => {
+        alex = new User({ name: 'Alex' });
+        maria = new User({ name: 'Maria' });
+        zach = new User({ name: 'Zach' });
         joe = new User({ name: 'Joe' });
-        joe.save().then(() => {
-            done();
-        });
+        Promise.all([alex.save(), maria.save(), zach.save(), joe.save()]).then(
+            () => {
+                done();
+            }
+        );
     });
 
     it('finds all users with a name of Joe', done => {
@@ -23,5 +28,18 @@ describe('Reading user out of the database', () => {
             assert(user.name === 'Joe');
             done();
         });
+    });
+
+    it('can skip and limit the result set', done => {
+        User.find({})
+            .sort({ name: 1 })
+            .skip(1)
+            .limit(2)
+            .then(users => {
+                assert(users.length === 2);
+                assert(users[0].name === 'Joe');
+                assert(users[1].name === 'Maria');
+                done();
+            });
     });
 });
